ba8ca95166d5e59f3e2799473bc95937a4d95ed5
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2022-02-23 20:49:16 +0900

    3주차 배열

---
+++ 3주차/const_push.js
@@ -0,0 +1,5 @@
+const todos = ['우유 구매', '업무 매일 확인', '필라테스 수업']
+console.log(todos)
+console.log(todos.push('저녁 식사 준비하기'))
+console.log(todos.push('피아노 연습하기'))
+console.log(todos)
---
+++ 3주차/const_test 01.js
@@ -0,0 +1,6 @@
+//const array = [273, 'String', true, function() { }, {}, [273, 10]]
+//console.log(array)
+
+const fruits = ['배', '사과', '키위', '바나나']
+console.log(fruits.length)
+console.log(fruits[fruits.length-1])
